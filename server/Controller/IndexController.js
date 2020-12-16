3239c98ebabb76b99aadc6f0dda68fa89296bc9d
phat97 <phat.le@hotmail.com>
2020-12-15 17:06:08 -0800

    Implemented Get all data and add data

--- server/Controller/IndexController.js
+++ server/Controller/IndexController.js
@@ -1,6 +1,6 @@
 // Handle endpoints
 const path = require("path");
-const gameDetailModel = require("../model/UserGameList");
+const { collection } = require("../model/UserGameList");
 
 exports.index = (req, res) => {
   res.sendFile(path.join(__dirname + "../build", "index.html")); // Load react page without data
@@ -8,11 +8,43 @@ exports.index = (req, res) => {
 
 // Get list of all Games
 exports.gameList = (req, res) => {
-  res.send("Not implemented: Return a list of all games");
+  collection
+    .find()
+    .then((result) => {
+      res.status(200).send(result);
+    })
+    .catch((err) => {
+      res.status(500).send(`Failed to retrieve all data: ${err}`);
+    });
+
+  res.send("This should never happen");
 };
 
 exports.gameAdd = (req, res) => {
-  res.send("Not implemented: Post a new game");
+  const data = req.body;
+  const query = { _userId: data._userId, _gameId: data._gameId };
+  const update = {
+    $set: {
+      _gameId: data._gameId,
+      title: data.type,
+      imageURI: data.imageURI,
+      own: data.own,
+      date: data.date,
+      players: data.players,
+    },
+  };
+  const options = { upsert: true };
+
+  collection
+    .updateOne(query, update, options)
+    .then((result) => {
+      res.status(200).send("Successfully added");
+    })
+    .catch((err) => {
+      res.status(500).send("Failed to add");
+    });
+
+  res.send("This should never happen");
 };
 
 exports.gameDelete = (req, res) => {
